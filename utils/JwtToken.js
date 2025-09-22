1bbfc7d3585766c4c6523c3e8cdd604ae7a1ecb5
RabiaAmin <rabia10march@gmail.com>
2025-09-22 23:20:02 +0200

    changes in jwtToken .js file

--- utils/JwtToken.js
+++ utils/JwtToken.js
@@ -1,19 +1,21 @@
-
 //this file is responsible for generating JWT tokens and sending them in the response
 export const generateToken = (user, message, statusCode, res) => {
-    const token = user.generateJsonWEbToken();
-  
-    res.status(statusCode)
-       .cookie("token", token, {
-           expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
-           httpOnly: true,
-           secure: false,
-           sameSite: "Lax",
-       })
-       .json({
-           success: true,
-           message,
-           token,
-           user
-       });
+  const token = user.generateJsonWEbToken();
+
+  res
+    .status(statusCode)
+    .cookie("token", token, {
+      expires: new Date(
+        Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
+      ),
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production", // ✅ true in prod, false in dev
+      sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // ✅ cross
+    })
+    .json({
+      success: true,
+      message,
+      token,
+      user,
+    });
 };
