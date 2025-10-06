4fb8c848aed9d31f0034892b8f378c2de4c823cd
RabiaAmin <rabia10march@gmail.com>
2025-10-06 18:56:40 +0200

    added vat applicablity in client level

--- model/client_collection.model.js
+++ model/client_collection.model.js
@@ -2,6 +2,14 @@ import mongoose from "mongoose";
 const clientSchema = new mongoose.Schema({
   name: { type: String, required: true },
   vatNumber: { type: String },
+  vatApplicable: {
+    type: Boolean,
+    default: true, // or false, depending on your common case
+  },
+  vatRate: {
+    type: Number,
+    default: 15, // in percentage
+  },
   registrationNumber: { type: String },
   address: { type: String },
   phone: { type: String },
